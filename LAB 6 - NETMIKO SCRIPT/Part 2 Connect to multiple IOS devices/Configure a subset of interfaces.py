e9c1e803ce04db25a5a9263cbd45961a659231b6
DmitriiIurev <125916500+DmitriiIurev@users.noreply.github.com>
2023-06-02 22:09:42 +0200

    Update Configure a subset of interfaces.py

--- LAB 6 - NETMIKO SCRIPT/Part 2 Connect to multiple IOS devices/Configure a subset of interfaces.py
+++ LAB 6 - NETMIKO SCRIPT/Part 2 Connect to multiple IOS devices/Configure a subset of interfaces.py
@@ -1,37 +1,32 @@
 from netmiko import ConnectHandler
-
 # Define the device information
-devices = [
-    {
-        'device_type': 'cisco_ios',
-        'ip': '10.125.100.187',
-        'username': 'admin',
-        'password': 'pxl',
-    },
-    {
-        'device_type': 'cisco_ios',
-        'ip': '10.125.100.181',
-        'username': 'admin',
-        'password': 'pxl',
-    },
-    # Add more devices as needed
-]
-# Configure a subset of interfaces
-def configure_subset_interfaces(devices, interface_commands):
-    for device in devices:
-        connection = ConnectHandler(**device)
-        print(f"\n==== Device: {device['ip']} ====")
-        output = connection.send_config_set(interface_commands)
-        print(f"Interface configuration applied:\n{output}")
-        connection.disconnect()
+device = {
+    'device_type': 'cisco_ios',
+    'ip': '10.125.100.187',
+    'username': 'admin',
+    'password': 'pxl',
+    'secret': 'cisco',
+}
 
-# Define interface configuration commands
-interface_commands = [
-    'interface GigabitEthernet0/1',
-    'shutdown',
-    'interface GigabitEthernet0/2',
-    'shutdown',
-]
+# Define the interfaces to configure
+interfaces = ['FastEthernet0/9', 'FastEthernet0/10', 'FastEthernet0/11']
 
-# Execute the functions
-configure_subset_interfaces(devices, interface_commands)
+# Define the configuration commands for the interfaces
+interface_config_commands = [
+    'description Configured by Netmiko',
+    'ip address 10.125.100.187',
+    'no shutdown',
+]
+# Establish an SSH connection to the device
+net_connect = ConnectHandler(**device)
+net_connect.enable()
+# Configure each interface in the subset
+for interface in interfaces:
+    # Enter interface configuration mode
+    config_commands = [f'interface {interface}']
+    output = net_connect.send_config_set(config_commands)
+    # Apply configuration commands for the interface
+    output += net_connect.send_config_set(interface_config_commands)
+    print(f"Configuration applied to interface {interface}:\n{output}")
+# Disconnect from the device
+net_connect.disconnect()
