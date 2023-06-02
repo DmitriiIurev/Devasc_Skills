ac5da2c2a97e7534108571e57f974aecd43d64b0
DmitriiIurev <125916500+DmitriiIurev@users.noreply.github.com>
2023-06-02 22:05:06 +0200

    Update Backup device configurations.py

--- LAB 6 - NETMIKO SCRIPT/Part 2 Connect to multiple IOS devices/Backup device configurations.py
+++ LAB 6 - NETMIKO SCRIPT/Part 2 Connect to multiple IOS devices/Backup device configurations.py
@@ -1,33 +1,49 @@
 from netmiko import ConnectHandler
-
+device = {
+    'device_type': 'cisco_ios',
+    'ip': '10.125.100.187',
+    'username': 'admin',
+    'password': 'pxl',
+    'secret': 'cisco',
+}
+net_connect = ConnectHandler(**device)
+net_connect.enable()
+output = net_connect.send_command('show running-config')
+filename = 'lab-ra08-a-sw03-confg'
+with open(filename, 'w') as f:
+    f.write(output)
+print(f"Device configuration saved to {filename}")
+net_connect.disconnect()
+- Configure a subset of Interfaces
+from netmiko import ConnectHandler
 # Define the device information
-devices = [
-    {
-        'device_type': 'cisco_ios',
-        'ip': '10.125.100.187',
-        'username': 'admin',
-        'password': 'pxl',
-    },
-    {
-        'device_type': 'cisco_ios',
-        'ip': '10.125.100.181',
-        'username': 'admin',
-        'password': 'pxl',
-    },
-    # Add more devices as needed
+device = {
+    'device_type': 'cisco_ios',
+    'ip': '10.125.100.187',
+    'username': 'admin',
+    'password': 'pxl',
+    'secret': 'cisco',
+}
+
+# Define the interfaces to configure
+interfaces = ['FastEthernet0/9', 'FastEthernet0/10', 'FastEthernet0/11']
+
+# Define the configuration commands for the interfaces
+interface_config_commands = [
+    'description Configured by Netmiko',
+    'ip address 10.125.100.187',
+    'no shutdown',
 ]
-# Backup device configurations
-def backup_configurations(devices):
-    for device in devices:
-        connection = ConnectHandler(**device)
-        print(f"\n==== Device: {device['ip']} ====")
-        output = connection.send_command('show running-config')
-        filename = f"{device['ip']}_config.txt"
-        with open(filename, 'w') as file:
-            file.write(output)
-        print(f"Configuration saved to {filename}")
-        connection.disconnect()
-
-
-# Execute the functions
-backup_configurations(devices)
+# Establish an SSH connection to the device
+net_connect = ConnectHandler(**device)
+net_connect.enable()
+# Configure each interface in the subset
+for interface in interfaces:
+    # Enter interface configuration mode
+    config_commands = [f'interface {interface}']
+    output = net_connect.send_config_set(config_commands)
+    # Apply configuration commands for the interface
+    output += net_connect.send_config_set(interface_config_commands)
+    print(f"Configuration applied to interface {interface}:\n{output}")
+# Disconnect from the device
+net_connect.disconnect()
