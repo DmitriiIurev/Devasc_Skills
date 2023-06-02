8d1373f221c120158cad9d59414d69e7bbe0c754
DmitriiIurev <125916500+DmitriiIurev@users.noreply.github.com>
2023-06-02 22:10:58 +0200

    Update Execute a script with a statements (if, ifelse, else).py

--- LAB 6 - NETMIKO SCRIPT/Part 2 Connect to multiple IOS devices/Execute a script with a statements (if, ifelse, else).py
+++ LAB 6 - NETMIKO SCRIPT/Part 2 Connect to multiple IOS devices/Execute a script with a statements (if, ifelse, else).py
@@ -1,40 +1,37 @@
 from netmiko import ConnectHandler
 
-# Define the device information
-devices = [
-    {
+def main():
+    device = {
         'device_type': 'cisco_ios',
         'ip': '10.125.100.187',
         'username': 'admin',
         'password': 'pxl',
-    },
-    {
-        'device_type': 'cisco_ios',
-        'ip': '10.125.100.181',
-        'username': 'admin',
-        'password': 'pxl',
-    },
-    
-]
-def execute_with_statements(device):
-    connection = ConnectHandler(**device)
-    print(f"Connected to {device['ip']}")
-    if device['ip'] == '10.125.100.187':
-        output = connection.send_command('show interfaces')
-        print(f"Output from {device['ip']}:\n{output}\n")
-    elif device['ip'] == '10.125.100.181':
-        output = connection.send_command('show ip route')
-        print(f"Output from {device['ip']}:\n{output}\n")
-    else:
-        print(f"No specific operation defined for {device['ip']}")
-    connection.disconnect()
+    }
 
+    try:
+        connection = ConnectHandler(**device)
+        print(f"Connected to {device['ip']} successfully.")
 
-for device in devices:
-    connect_with_dictionary(device)
+        # Send show command to retrieve interface status
+        output = connection.send_command('show interface GigabitEthernet0/1')
+        if 'GigabitEthernet0/1 is up' in output:
+            print("Interface GigabitEthernet0/1 is up and operational.")
+        else:
+            print("Interface GigabitEthernet0/1 is down or not operational.")
 
-device1 = CiscoDevice('cisco_ios', '10.125.100.181', 'admin', 'pxl')
-device1.connect()
+        # Send show command to retrieve interface speed
+        output = connection.send_command('show interface GigabitEthernet0/1 | include BW')
+        speed = int(output.split()[2])
+        if speed > 1000:
+            print("Interface speed is greater than 1000 Mbps.")
+        else:
+            print("Interface speed is less than or equal to 1000 Mbps.")
 
-execute_with_statements(devices[0])
-execute_with_statements(devices[1])
+        connection.disconnect()
+        print(f"Disconnected from {device['ip']}.\n")
+
+    except Exception as e:
+        print(f"Failed to connect to {device['ip']}. Error: {str(e)}\n")
+
+if __name__ == '__main__':
+    main()
